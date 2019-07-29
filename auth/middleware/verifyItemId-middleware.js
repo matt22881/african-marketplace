f865f8874e8457a1dd0d8d0c9461aee0205e3467
Kevin Sims <kevinsims1@outlook.com>
2019-07-29 16:15:23 -0700

    end points set up

---
+++ auth/middleware/verifyItemId-middleware.js
@@ -0,0 +1,16 @@
+module.exports = function verifyItemId(req, res, next) {
+    const id = req.params.id;
+  
+    Items.getItemsById(id)
+      .then(item => {
+        if (item) {
+          req.item = item;
+          next();
+        } else {
+          res.status(404).json({ message: "Item doesn't exist." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  }
--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const itemsRouter = require('../items/items-router.js')
 
 module.exports = server => {
     server.use('/auth', authRouter);
-    server.use('/marketplace', usersRouter);
+    server.use('/users', usersRouter);
+    server.use('/items', itemsRouter);
   };
