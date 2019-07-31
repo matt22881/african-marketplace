8941e14215b2a78acb1d4c1a10f80905976b521a
Kevin Sims <kevinsims1@outlook.com>
2019-07-31 10:18:59 -0700

    seed fixes

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -1,9 +1,9 @@
 
 exports.seed = function(knex) {
     return knex('users').insert([
-      {id: 1, username: 'Melanie', password: "Testing", department: "buyer"},
-      {id: 2, username: 'Bryan', password: "Testing", department: "buyer"},
-      {id: 3, username: 'Nikki', password: "Testing", department: "seller"},
-      {id: 4, username: 'Marcus', password: "Testing",  department: "seller"}
+      {id: 3, username: 'Melanie', password: "Testing", department: "buyer"},
+      {id: 7, username: 'Bryan', password: "Testing", department: "buyer"},
+      {id: 8, username: 'Nikki', password: "Testing", department: "seller"},
+      {id: 9, username: 'Marcus', password: "Testing",  department: "seller"}
     ]);
 };
