10a0da85402d5b571fbebd25923dc10d55719497
Kevin Sims <kevinsims1@outlook.com>
2019-07-31 10:36:18 -0700

    auth-router fixes

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
     return knex('users').insert([
-      {id: 3, username: 'Melanie', password: "Testing", department: "buyer"},
+      {id: 10, username: 'Melanie', password: "Testing", department: "buyer"},
       {id: 7, username: 'Bryan', password: "Testing", department: "buyer"},
       {id: 8, username: 'Nikki', password: "Testing", department: "seller"},
       {id: 9, username: 'Marcus', password: "Testing",  department: "seller"}
