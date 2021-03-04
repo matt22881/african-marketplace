1b0601d5e498e7a3da8721424cb1596ce8cf5c9d
matt22881 <matt22881@gmail.com>
2021-03-04 14:52:48 -0500

    change user seeds to minimize confusion

--- database/seeds/01_users.js
+++ database/seeds/01_users.js
@@ -1,13 +1,7 @@
 
 exports.seed = function(knex) {
     return knex('users').insert([
-      {username: 'Austin1', password: 'Testing', department: 'seller'},
-      {username: 'Matt1', password: 'Testing', department: 'seller'},
-      {username: 'Hamida1', password: 'Testing', department: 'seller'},
-      {username: 'Jonathan1', password: 'Testing',  department: 'seller'},
-      {username: 'Austin2', password: 'Testing', department: 'buyer'},
-      {username: 'Matt2', password: 'Testing', department: 'buyer'},
-      {username: 'Hamida2', password: 'Testing', department: 'buyer'},
-      {username: 'Jonathan2', password: 'Testing',  department: 'buyer'}
+      {username: 'testseller', password: 'selling', department: 'seller'},
+      {username: 'testbuyer', password: 'buying',  department: 'buyer'}
     ]);
 };
